e14da4580a0f7116cd92c946518acc14ef438d83
Stolar <stolar28@gmail.com>
2019-04-21 19:43:35 +0200

    added mocker with extensions class

---
+++ tests/CrcAspNetCore.UnitTests/BookContextMocker.cs
@@ -0,0 +1,21 @@
+﻿using CrcAspNetCore.Api.DbContexts;
+using CrcAspNetCore.Api.Repositories;
+using Microsoft.EntityFrameworkCore;
+
+namespace CrcAspNetCore.UnitTests
+{
+    public static class BookContextMocker
+    {
+        public static IBookRepository GetInMemoryMeasurementsRepository(string dbName)
+        {
+            var options = new DbContextOptionsBuilder<BookContext>()
+                .UseInMemoryDatabase(databaseName: dbName)
+                .Options;
+
+            var bookContext = new BookContext(options);
+            bookContext.FillDatabase();
+
+            return new BookRepository(bookContext);
+        }
+    }
+}
