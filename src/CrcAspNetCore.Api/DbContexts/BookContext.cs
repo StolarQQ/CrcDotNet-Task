2e3c66626aa0be25daa1592f2d1fd6214fee2ca6
Stolar <stolar28@gmail.com>
2019-04-21 16:38:00 +0200

    added book db context

---
+++ src/CrcAspNetCore.Api/DbContexts/BookContext.cs
@@ -0,0 +1,15 @@
+﻿using CrcAspNetCore.Api.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CrcAspNetCore.Api.DbContexts
+{
+    public class BookContext : DbContext
+    {
+        public BookContext(DbContextOptions options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Book> Books { get; set; }
+    }
+}
